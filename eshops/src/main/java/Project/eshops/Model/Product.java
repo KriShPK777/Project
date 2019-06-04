c04a239a4bfb25fee3f5b995ee70ac0720afc425
Pragadish Krishna <krishnalosblancos787@gmail.com>
2019-06-04 15:23:16 +0530

    Updated Product.java file

--- eshops/src/main/java/Project/eshops/Model/Product.java
+++ eshops/src/main/java/Project/eshops/Model/Product.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table
@@ -20,6 +21,9 @@ public class Product<MultipartFile>
 	int price;
 	int categoryId;
 	int supplierId;
+	
+	@Transient
+	MultipartFile pimage;
 	
 	public int getProductId() {
 		return productId;
